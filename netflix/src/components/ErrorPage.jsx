c131deda68f3e13f6b12c627482eb5ddb448fc58
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-01-13 08:19:20 +0000

    heading removed

--- netflix/src/components/ErrorPage.jsx
+++ netflix/src/components/ErrorPage.jsx
@@ -1,8 +1,7 @@
 
 const ErrorPage = () => {
-    return(<div className='text-center'>
-        <h1 >404 page not found</h1>
-        <img src='https://i.pinimg.com/originals/fd/78/c4/fd78c47f2a009df65b5b5a46f4437399.png' alt=''/>
+    return(<div className='text-center p-0 m-0'>
+        <img src='https://i.pinimg.com/originals/fd/78/c4/fd78c47f2a009df65b5b5a46f4437399.png' alt='error page'/>
     </div >
     )
 }
