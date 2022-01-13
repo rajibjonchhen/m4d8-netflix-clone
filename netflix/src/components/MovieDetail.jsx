e7a5930ef9f55fff5b2b3497dcebb5bf046d7ca4
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-01-13 08:07:56 +0000

    movie details displayed

--- netflix/src/components/MovieDetail.jsx
+++ netflix/src/components/MovieDetail.jsx
@@ -22,8 +22,7 @@ const MovieDetail = () => {
 
             let movieRes = await fetch('http://www.omdbapi.com/?apikey=15c1c355&i=' + movieId)
             let data = await movieRes.json()
-
-            if (movieRes.ok) {
+              if (data) {
                setMovie(data)
                 setIsLoading(false)
                 console.log(movie)
@@ -41,10 +40,10 @@ const MovieDetail = () => {
         {isLoading && <Loading/>}
         <h1>Movie Details</h1>
         <Card className='px-1 py-4 mb-4 m-0 border-0 w-100 bg-transparent' style={{maxWidth:"18rem"}}>
-                <Card.Img className="img-poster align-self-center" variant="top" src='/' />
+                <Card.Img className="img-poster align-self-center" variant="top" src={movie.Poster} />
                 <div className="position-relative">
                     <Card.Body className="d-flex flex-column pb-0 pt-2 px-0 justify-content-between w-100 position-absolute">
-                        {/* <Card.Title className="mb-2 px-2"> <h6>{movie.Title}</h6> <h6>{movie.imdbID}</h6></Card.Title> */}
+                        <Card.Title className="mb-2 px-2"> <h6>{movie.Title}</h6> <h6>{movie.imdbID}</h6></Card.Title>
                     </Card.Body>
                 </div>
             </Card>
